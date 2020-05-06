76da689e3ed3e99509de210e4703a8ee8d98fcc5
s3618696 <s3618696@rmit.edu.vn>
2020-05-06 13:34:42 +0700

    [ADD] setup a3

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,14 @@
 /* Insert your javascript here */
+function openBooking() {
+  document.getElementById("blur-backgroud").style.display = "block";
+  document.getElementById("movie-detail").style.display = "block";
+}
+window.onload = function () {
+  var hideMovie = document.getElementById("movie-detail");
+  document.onclick = function (e) {
+    if (e.target.id == "blur-backgroud") {
+      hideMovie.style.display = "none";
+      document.getElementById("blur-backgroud").style.display = "none";
+    }
+  };
+};
